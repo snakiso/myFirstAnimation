5d8d4348efc8d9522dc8c58596c37db7c9880082
igorchagov <igorchagov@mail.ru>
2022-07-04 09:15:11 +0300

    change some js

--- data.js
+++ data.js
@@ -13,55 +13,59 @@ function progressBar() {
 }
 window.addEventListener('scroll', progressBar);
 
-function debounce(func, timeout = 300) {
+function debounce(func, timeout = 100) {
   let timer;
   return (...args) => {
     clearTimeout(timer);
     timer = setTimeout(() => { func.apply(this, args); }, timeout);
   };
 }
+
 function changeClass(first, second, third){
   first.classList.remove(isActive)
   second.classList.add(isActive)
   third.classList.remove(isActive)
 }
 
-window.addEventListener('scroll', function(){
-  console.log (this.window.pageYOffset)
- if (this.window.pageYOffset < 100 ){
-  list.style.transform = 'translateY(0px)'
-   changeClass(date[1], date[0], date[2])
- }
-  if (this.window.pageYOffset >= 100 ){
-   list.style.transform = 'translateY(-230px)'
-   changeClass(date[0],date[1],date[2])
- } 
-  if (this.window.pageYOffset >= 800 ){
-   list.style.transform = 'translateY(-460px)'
+const applyAnimation = () => {
+  console.log(this.window.pageYOffset)
+  if (this.window.pageYOffset < 100) {
+    list.style.transform = 'translateY(0px)'
+    changeClass(date[1], date[0], date[2])
+  }
+  if (this.window.pageYOffset >= 100) {
+    list.style.transform = 'translateY(-230px)'
+    changeClass(date[0], date[1], date[2])
+  }
+  if (this.window.pageYOffset >= 800) {
+    list.style.transform = 'translateY(-460px)'
     changeClass(date[1], date[2], date[3])
- }
-  if (this.window.pageYOffset >= 1500 ) {
-  list.style.transform = 'translateY(-690px)'
-   changeClass(date[2], date[3], date[4])
- }
+  }
+  if (this.window.pageYOffset >= 1500) {
+    list.style.transform = 'translateY(-690px)'
+    changeClass(date[2], date[3], date[4])
+  }
   if (this.window.pageYOffset >= 2200) {
-  list.style.transform = 'translateY(-920px)'
-   changeClass(date[3], date[4], date[5])
- }
-  if (this.window.pageYOffset > 3440 ) {
-  list.style.transform = 'translateY(-1150px)'
-   changeClass(date[4], date[5], date[6])
- }
-  if (this.window.pageYOffset > 5100  ) {
-  list.style.transform = 'translateY(-1380px)'
-   changeClass(date[5], date[6], date[7])
- }
-if (this.window.pageYOffset > 5810 ) {
-  list.style.transform = 'translateY(-1610px)'
-   changeClass(date[6], date[7], date[8])
- }
-if (this.window.pageYOffset > 7000 ) {
-  list.style.transform = 'translateY(-1840px)'
-   changeClass(date[7], date[8], date[7])
- }
-})
+    list.style.transform = 'translateY(-920px)'
+    changeClass(date[3], date[4], date[5])
+  }
+  if (this.window.pageYOffset > 3440) {
+    list.style.transform = 'translateY(-1150px)'
+    changeClass(date[4], date[5], date[6])
+  }
+  if (this.window.pageYOffset > 5100) {
+    list.style.transform = 'translateY(-1380px)'
+    changeClass(date[5], date[6], date[7])
+  }
+  if (this.window.pageYOffset > 5810) {
+    list.style.transform = 'translateY(-1610px)'
+    changeClass(date[6], date[7], date[8])
+  }
+  if (this.window.pageYOffset > 7000) {
+    list.style.transform = 'translateY(-1840px)'
+    changeClass(date[7], date[8], date[7])
+  }
+}
+
+const debouncedApplyAnimation = debounce(applyAnimation)
+window.addEventListener('scroll', debouncedApplyAnimation)
